88dc9564c2d22e233a9d8b760589296af603ae9e
Adam Fanello <kernwig@gmail.com>
2019-02-03 16:35:37 -0800

    first commmit

---
+++ README.md
@@ -0,0 +1,74 @@
+# G-Drive Don't Keep!
+
+Google drive has a hidden bug. I see in on every account, but Google
+support calls it a feature, while contradicting the official documentation.
+
+What is this problem? **It never deletes anything!**
+
+When you edit or overwrite a (not G-Suite) file, it is supposed to keep
+the old revision of this file for 30 days before purging it. This lets
+you revert back to the old revision. Most people don't even knows these
+automatic backups are there, but they do count against your usage quota!
+
+Fine. Dandy. But there's a bug. You see there's also a feature to go
+make a revision as "Keep Forever", if you want. The bug is, _that option
+is automatically set_! So while the documentation tells you that old
+revisions will be deleted after 30 days, freeing up your disk quota,
+they actually are _never deleted_!
+
+Now, this does not happen with all files or all ways files are updated.
+I haven't figured out the pattern. I can tell you two examples:
+
+1. Upload a text file via https://drive.google.com. 
+   - Edit the file locally, and upload it again.
+2. Use Google Backup and Sync
+   - Include an MP3 file in your local Google Drive folder. (Let it sync up.)
+   - Edit that MP3 (change tags). (Let it sync.)
+   
+In both cases, you will find two revisions - the current one and the older
+one with the _Keep Forever_ checkmark hidden within.
+
+![Guide](./README.png)
+
+I discovered this when Google Backup and Sync told me that my data
+was using 59 GB, but drive.google.com said I was using 108 GB.
+
+Over going around in circles with Google support, I found the 
+[Google Drive API](https://developers.google.com/drive/api/v3/about-sdk)
+and wrote this script.
+
+No run, clone this repository, cd into it, and run:
+(Node.js v8 or later is required).
+
+```
+ npm install
+ npm start
+```
+
+
+Follow the directions to authorize the application to access your
+Google account - this will be saved to the `token.json` file and
+remain valid for some times, so that you don't have to authorize
+again if you run again soon.
+
+This script takes a _very_ long time to run. It has to issue a
+request to Google for every single file in your G-Drive, to check
+the revisions on it, and another for to clear each _Keep Forever_ 
+flags it finds. Plan to let it run over night - maybe longer.
+
+Until Google fixes this behavior, you will need to re-run on occation
+to find new revisions and clear the flag on them.
+
+---
+
+Using Node.js is an unreasonable requirement for most people. I wrote
+this script to solve my own account, and share it _as open source_ for
+anyone else to use. If you can make it more user friendly, please do.
+Let me know about it and I'll link to your project.
+
+LEGAL DISCLAIMER: This script works for me, but I make no guarantees
+what so ever that it will work for you. Some of your data will be
+deleted. That is the purpose of this script. It should only be the
+non-current historical revisions of files that were replaced over
+30 days ago.
+The source code is here, and you run it yourself. Use at your own risk. 
---
+++ index.ts
@@ -0,0 +1,134 @@
+import 'source-map-support/register'
+import * as fs from 'fs';
+import * as readline from 'readline';
+import {google} from 'googleapis';
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Drive API.
+    authorize(JSON.parse(content.toString()), processFiles);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getAccessToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token.toString()));
+        callback(oAuth2Client);
+    });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getAccessToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error retrieving access token', err);
+            oAuth2Client.setCredentials(token);
+            // Store the token to disk for later program executions
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) console.error(err);
+                console.log('Token stored to', TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+        });
+    });
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function processFiles(auth) {
+    try {
+        const drive = google.drive({version: 'v3', auth});
+        let nextPageToken;
+
+        do {
+            const pageSize = 100;
+            console.log(`Checking next ${pageSize} files...`);
+            const res = await drive.files.list({
+                pageSize,
+                fields: 'nextPageToken, files(id, name, ownedByMe, mimeType)',
+                q: "trashed = false",
+                pageToken: nextPageToken,
+            });
+            nextPageToken = res.data.nextPageToken;
+
+            const files = res.data.files as any[];
+            if (files.length) {
+                for (const file of files) {
+                    if (file.ownedByMe && file.mimeType !== "application/vnd.google-apps.folder") {
+                        //console.log(`${file.name} (${file.id})`);
+                        await getRevisions(drive, file);
+                    }
+                }
+            } else {
+                console.log('No more files found.');
+                break;
+            }
+        } while (nextPageToken);
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+async function getRevisions(drive, file): Promise<void> {
+    try {
+        const res = await drive.revisions.list({fileId: file.id, fields: '*'});
+        for (const rev of res.data.revisions) {
+            // console.log(`\trev ${rev.modifiedTime} keepForever=${rev.keepForever}`);
+            if (rev.keepForever) {
+                //await cleanRevision(drive, file, rev);
+            }
+        }
+    }
+    catch (err) {
+        if (err.message === 'The file does not support revisions.') {
+            console.warn(err.toString(), JSON.stringify(file));
+        }
+    }
+}
+
+async function cleanRevision(drive, file, revision): Promise<void> {
+    const res = await drive.revisions.update({
+        fileId: file.id,
+        revisionId: revision.id,
+        requestBody: {
+            keepForever: false
+        }
+    });
+    console.log(`Cleared keep-forever on "${file.name}" revision ${revision.id}`);
+}
